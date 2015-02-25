f018c1076ff9ccdca278167a2f4b012758660d63
Christian Nunez <cnunez@brandeis.edu>
2015-02-25 13:42:53 -0500

    Revert "some comment about changes made"
    This reverts commit 54ee1a5b09b9a66803ffe594cf46df9e62ca79e3.

--- public/students/vanio/vanio.js
+++
@@ -1,9 +0,0 @@
-function calc_age(){
-	console.log("pushed the button!!");
-	console.log("Your age in years is "+ $("#ageInYears").val())
-	var inputVal = $("#ageInYears").val();
-	var ageInYears = parseFloat(inputVal);
-	var ageInSeconds = ageInYears*365.25*24*60*60;
-	var ageInMegaSeconds = ageInSeconds/1000000;
-	$("#ageInSeconds").val(ageInMegaSeconds);
-}
